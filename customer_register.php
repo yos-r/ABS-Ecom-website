85b85e6ecc3bfe0e62029dc6effbc11fbd8838f0
Yosr <84140616+yos-r@users.noreply.github.com>
2022-04-04 18:50:20 +0100

    deleted old images folder and added the revamped index.php

--- customer_register.php
+++ customer_register.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start();
-    include("includes/connection.php");
-    include("includes/head.php");
-    include("includes/functions.php");
-    include("includes/main.php");
-?>
+    require_once("includes/connection.php");
+    require_once("includes/head.php");
+    require_once("includes/functions.php");
+    require_once("includes/main.php");
+?>2j
 <div id="content" >
 <div class="container" >
 <div class="col-md-12">
@@ -16,20 +16,24 @@ session_start();
 
     <form action="customer_register.php" method="post" enctype="multipart/form-data" ><!-- form Starts -->
         <div class="form-group" >
-            <label>Customer Name</label>
+            <label> Nom</label>
             <input type="text" class="form-control" name="c_name" required>
         </div>
         <div class="form-group">
-            <label> Customer Email</label>
+            <label>  Email</label>
             <input type="text" class="form-control" name="c_email" required>
         </div>
+        <div class="form-group">
+            <label>  Adresse de livraison</label>
+            <input type="text" class="form-control" name="c_address" required>
+        </div>
         <div class="form-group"><!-- form-group Starts -->
-            <label> Customer Password </label>
+            <label> Mot de passe </label>
             <input type="password" class="form-control" id="pass" name="c_pass" required>
         </div>
         <div class="text-center"><!-- text-center Starts -->
             <button type="submit" name="register" class="btn btn-primary">
-                Register
+                S'inscrire
             </button>
         </div>
     </form>
@@ -44,26 +48,27 @@ if(isset($_POST['register'])){
     $c_name = $_POST['c_name'];
     $c_email = $_POST['c_email'];
     $c_pass = $_POST['c_pass'];
+    $c_address=$_POST['c_address'];
     $c_ip = getRealUserIp();
     $get_email = "select * from customers where customer_email='$c_email'";
     $run_email = mysqli_query($con,$get_email);
     $check_email = mysqli_num_rows($run_email);
     if($check_email == 1){
-        echo "<script>alert('This email is already registered, try another one')</script>";
+        echo "<script>alert('cet email existe déja')</script>";
     exit();
-    $insert_customer = "insert into customers (customer_name,customer_email,customer_pass) values ('$c_name','$c_email','$c_pass','$c_country','$c_city','$c_contact','$c_address','$c_image','$c_ip','$customer_confirm_code')";
+    $insert_customer = "insert into customers (customer_name,customer_email,customer_pass,customer_address) values ('$c_name','$c_email','$c_pass','$c_address')";
     $run_customer = mysqli_query($con,$insert_customer);
     $sel_cart = "select * from cart where ip_add='$c_ip'";
     $run_cart = mysqli_query($con,$sel_cart);
     $check_cart = mysqli_num_rows($run_cart);
     if($check_cart>0){
       $_SESSION['customer_email']=$c_email;
-        echo "<script>alert('You have been Registered Successfully')</script>";
+        echo "<script>alert('Vous êtes inscrit(e)!')</script>";
         echo "<script>window.open('checkout.php','_self')</script>";
       }
       else{
         $_SESSION['customer_email']=$c_email;
-        echo "<script>alert('You have been Registered Successfully')</script>";
+        echo "<script>alert('Vous êtes inscrit(e)!!')</script>";
         echo "<script>window.open('index.php','_self')</script>";
       }
     
--- includes/head.php
+++ includes/head.php
@@ -2,8 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Construction store website</title>
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--the browser will render the width of the page at the width of its own screen-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>  
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+        crossorigin="anonymous" />
+    <title>ABS</title>
 </head>
