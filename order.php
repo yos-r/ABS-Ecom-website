b3eaae99189f934a3178a2a4911b7c40e9299a50
Yosr <84140616+yos-r@users.noreply.github.com>
2022-04-04 21:09:00 +0100

    Update order.php

--- order.php
+++ order.php
@@ -2,7 +2,6 @@
 include("includes/connection.php");
 include("includes/head.php");
 include("includes/functions.php");
-include("includes/main.php");
 ?>
 <?php
     if(isset($_GET['c_id'])){
@@ -25,3 +24,22 @@ include("includes/main.php");
         echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
     }
 ?>
+
+
+<!--
+    may get used for report? or order details? 
+$i = 0;
+$ip_add = getRealUserIp();
+$get_cart = "select * from cart where ip_add='$ip_add'";
+$run_cart = mysqli_query($con,$get_cart);
+while($row_cart = mysqli_fetch_array($run_cart)){
+    $pro_id = $row_cart['p_id'];
+    $pro_qty = $row_cart['qty'];
+    $pro_price = $row_cart['p_price'];
+    $get_products = "select * from products where product_id='$pro_id'";
+    $run_products = mysqli_query($con,$get_products);
+    $row_products = mysqli_fetch_array($run_products);
+    $product_title = $row_products['product_title'];
+    $i++;
+
+*/-->
