e3253c6fc1d51cb8a9f7263556d9007ce8a87692
Yosr Barghouthi <yosrbargouti@gmail.com>
2022-04-17 17:01:56 +0100

    live search enabled for customer

--- customer/requires/head.php
+++ customer/requires/head.php
@@ -10,4 +10,24 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
         crossorigin="anonymous" />
     <title>ABS</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        $('.search-box input[type="text"]').on("keyup input", function(){
+            var inputVal = $(this).val();
+            var resultDropdown = $(this).siblings(".result");
+            if(inputVal.length){ 
+                $.get("livesearch.php", {term: inputVal}).done(function(data){
+                    resultDropdown.html(data);
+                });
+            } else{
+                resultDropdown.empty();
+            }
+        });
+        $(document).on("click", ".result a", function(){
+            $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+            $(this).parent(".result").empty();
+        });
+    });
+    </script> <!--fin script de recherche -->
 </head>
